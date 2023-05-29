028ef0713e2169bebd6d8cdcde010063792a5d0f
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-05-29 11:34:21 +0200

    init commit

---
+++ pierwszy.py
@@ -0,0 +1,29 @@
+# PEP8 - zasady czystego kodu
+# ctrl alt l
+# 10:40
+print()  # wydrukuj/wypisz
+print("Nazywam się Radek")  # str - teksty
+print('Nazywam się Radek')
+print('Nazywam się Radek')
+# ctrl d - kopiowanie linii
+print(39)  # int - liczby całkowite
+
+print(type(39))  # wypisanie typu danej, <class 'int'>
+print(type("Radek"))  # <class 'str'>
+print("39" + "15")  # 3915
+print(39 + 15)  # 54
+print(5 * "4")  # 44444
+
+# zmienna - pudełko na dane
+imie = "Radek"
+print(type(imie))  # <class 'str'>
+print(imie)  # wyswietlenie zawartosci zmiennej
+
+wiek = 48
+print(wiek)
+print(type(wiek))  # <class 'int'>
+
+wiek = "Radek"
+print(type(wiek))  # <class 'str'>
+print(wiek)
+# 11:30
