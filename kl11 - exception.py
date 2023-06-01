7dc16ba7cfd3c4b0ae7efdb9eece885e6c9575c8
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-06-01 10:53:29 +0200

    addd kl11

---
+++ kl11 - exception.py
@@ -0,0 +1,11 @@
+class MyException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+try:
+    raise MyException("wystąpił bład")  # MyException: wystąpił bład
+except Exception as e:
+    print(e)  # wystąpił bład
+
+# 11:20
